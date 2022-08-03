1b58077b4b7cd43f47d921c8d416c2101baa51ce
BilAh01 <110549670+BilAh01@users.noreply.github.com>
2022-08-03 20:43:02 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,16 +33,16 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
+global.owner = ['94788503314',''] //owner number +94788503314
+global.wame = ['94788503314'] 
+global.pemilik = ['94788503314'] //owner number +94788503314
+global.premium = ['94788503314'] //premium number +94788503314
+global.pengguna = 'RPKING' //username
 global.footer = 'ZIM BOT INC'
 global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
+global.botname = 'RPKING'
+global.ownernma = 'Rpking' //owner name
+global.packname = 'Rpking' //sticker package name
 global.author = 'By Drips Memes' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
